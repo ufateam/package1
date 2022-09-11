1122efc3f2a20be1b89b0861e0be60c985e68bda
UFA Team1 <ufateam1@gmail.com>
2022-09-11 18:09:40 +0700

    first commit

---
+++ spec/package1-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import Package1View from '../lib/package1-view';
+
+describe('Package1View', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
